846b5b5bc68766a0b984a9b37d4b0e4e23a0787f
Willian <willian.alves.b15@gmail.com>
2022-10-31 09:59:59 -0300

    feat: implementando analyze_log

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,17 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    formato_csv = str(path_to_file).split(".")[-1]
+    if formato_csv != "csv":
+        raise FileNotFoundError(f"Extensão inválida:'{path_to_file}'")
+    try:
+        with open(path_to_file, encoding="utf8") as arquivo:
+            ordens = []
+            dados = csv.reader(arquivo, delimiter=",", quotechar='"')
+            *data, = dados
+            for orden in data:
+                ordens.append(orden)
+            return ordens
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente:'{path_to_file}'")
